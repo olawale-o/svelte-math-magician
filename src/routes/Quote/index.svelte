a1a80ff82bd2a5d5ee2f581b0f9b936533c56ecb
olawale-o <omoogunolawale@gmail.com>
2022-07-11 22:59:59 +0100

    Create quote route

---
+++ src/routes/Quote/index.svelte
@@ -0,0 +1,25 @@
+<script>
+  import { useFocus } from 'svelte-navigator';
+  const registerFocus = useFocus();
+</script>
+
+<div class="quote">
+  <p data-testid="quote" use:registerFocus>
+    Mathematics is not about numbers, equations, computations
+    <br />
+    or algorithms: it is about understanding. -William Paul Thurston
+  </p>
+</div>
+
+<style>
+  .quote {
+    padding: 15px;
+  }
+
+  .quote p {
+    font-size: 3.5rem;
+    font-weight: light;
+    margin: 10rem auto;
+    width: 85%;
+  }
+</style>
