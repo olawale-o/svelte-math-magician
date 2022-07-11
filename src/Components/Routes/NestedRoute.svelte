e52c4ac1aaf2993c316400c9271014bbe4d64adc
olawale-o <omoogunolawale@gmail.com>
2022-07-11 23:01:46 +0100

    Create Nested and Single route

---
+++ src/Components/Routes/NestedRoute.svelte
@@ -0,0 +1,16 @@
+<script>
+  import { Route } from 'svelte-navigator';
+  import SingleRoute from './SingleRoute.svelte';
+  export let routes;
+  let props;
+  $: {
+    const {routes, ...restProps } = $$props;
+    props = restProps;
+  }
+</script>
+
+<Route {...props}>
+  {#each routes as route}
+    <SingleRoute {...route} />
+  {/each}
+</Route>
---
+++ src/Components/Routes/SingleRoute.svelte
@@ -0,0 +1,13 @@
+<script>
+  import { Route } from 'svelte-navigator';
+  export let component;
+  let props;
+  $: {
+    const {component, ...restProps } = $$props;
+    props = restProps;
+  }
+</script>
+
+<Route {...props}>
+  <svelte:component this={component} />
+</Route>
