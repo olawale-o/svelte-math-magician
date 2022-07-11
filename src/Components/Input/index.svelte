a4a5c1cc2fefcde023f998688689d8285a2fab04
olawale-o <omoogunolawale@gmail.com>
2022-07-11 23:02:05 +0100

    Create Input component

---
+++ src/Components/Input/index.svelte
@@ -0,0 +1,10 @@
+<script>
+  export let inputValue = '';
+  const handleOnChange = (event) => {
+    inputValue = event.target.value;
+  };
+</script>
+
+<div class="field">
+  <input class="input" value={inputValue} on:input={handleOnChange} disabled />
+</div>
