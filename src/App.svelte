2ef2a3d0f259695c1cbe3f22b3aec4a7ce4a2699
olawale-o <omoogunolawale@gmail.com>
2022-07-13 21:32:36 +0100

    Use custom routes

--- src/App.svelte
+++ src/App.svelte
@@ -1,8 +1,23 @@
 <script>
+  import { Router } from 'svelte-navigator';
+  import Header from './Components/Header/Index.svelte';
+  import SingleRoute from './Components/Routes/SingleRoute.svelte';
+  import NestedRoute from './Components/Routes/NestedRoute.svelte';
+  export let routes;
 </script>
 
-<main>
-</main>
+<Router>
+  <Header />
+  <div id="app">
+    {#each routes as route}
+      {#if route.routes}
+        <NestedRoute {...route} />
+      {:else}
+        <SingleRoute {...route} />
+      {/if}
+    {/each}
+  </div>
+</Router>
 
 <style>
 </style>
