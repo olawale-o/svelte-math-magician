c171eda3db9a9adaa822c678c49ec3916d38f7bd
olawale-o <omoogunolawale@gmail.com>
2022-07-13 22:28:23 +0100

    Implement calculator functionality

--- src/Components/Button/index.svelte
+++ src/Components/Button/index.svelte
@@ -1,5 +1,13 @@
 <script>
+  import { createEventDispatcher } from 'svelte';
   export let cssClass, text;
+  const dispatch = createEventDispatcher();
+
+  function handleCalc(e) {
+    dispatch('calc', {
+      text: e.target.textContent,
+    });
+  }
 </script>
 
-<button type="button" class={cssClass}>{text}</button>
+<button type="button" class={cssClass} on:click={handleCalc}>{text}</button>
